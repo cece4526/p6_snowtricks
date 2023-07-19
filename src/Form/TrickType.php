235add6d3a0420a492f8a083b938c04acbc1db7b
cece4526 <cece4526@hotmail.fr>
2023-07-19 11:14:09 +0200

    created form for video, immage and trick

---
+++ src/Form/TrickType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tricks;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+            ->add('description')
+            ->add('updateAt')
+            ->add('mainImageName')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tricks::class,
+        ]);
+    }
+}
